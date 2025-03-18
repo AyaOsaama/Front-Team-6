c2ab86c31540ace208c0c2c77358ce957bca22c1
MohammedKabil2021 <Mohamedkabil54@gmail.com>
2025-03-18 13:25:37 +0200

    Creation CO work Kabil_Farha

---
+++ src/Pages/Kabil&Farha/Kabil_Farha.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Kabil_Farha() {
+    return (
+        <>
+            <div>Kabil&Farha</div>
+            <p>You are welcome farha</p>
+        </>
+    )
+}
+
+export default Kabil_Farha
