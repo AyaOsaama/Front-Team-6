0093d6b9014c02aa257dcc70335505c0f89f0cb6
Aya Osama <ayaosama40a@gmail.com>
2025-03-18 12:18:41 +0200

    Add Home Page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Home</h1>
+        <p>Welcome to the Home page! Create By Aya</p>
+    </div>
+  )
+}
+
+export default Home
