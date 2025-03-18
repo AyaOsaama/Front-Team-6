e29a0a1eef34828803bd8772bf29191bf0602d5c
MostafaGaber135 <mostafagaber1234560@gmail.com>
2025-03-18 13:05:24 +0200

    Add Mostafa Page

---
+++ src/Pages/Mostafa/Mostafa.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Mostafa() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the Mostafa page! Create By Mostafa</p>
+    </div>
+  );
+}
+
+export default Mostafa;
