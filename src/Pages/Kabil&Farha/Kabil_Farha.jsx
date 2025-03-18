b150078f88ed4cc19be6bda119c6c4d2b0d22a4f
farha_eldaba <farhaeldaba@gmail.com>
2025-03-18 13:36:06 +0200

    pdate

---
+++ src/Pages/Kabil&Farha/Kabil_Farha.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Kabil_Farha() {
+    return (
+        <>
+            <div>Kabil&Farha</div>
+            <p>You are welcome farha</p>
+            <div>Kabil&Farha</div>
+            <p>You are welcome farha</p>
+
+        </>
+    )
+}
+
+export default Kabil_Farha
